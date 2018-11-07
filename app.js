dcf401e909bb2cf8cf715d9dfdfba9332539243f
Deise Trianon <39528989+deisetrianon@users.noreply.github.com>
2018-11-07 13:24:19 -0200

    Refactoring app.js

--- app.js
+++ app.js
@@ -1,34 +1,10 @@
-var insertCardNumbers = prompt("Insira o número do seu cartão de crédito.");
-if (insertCardNumbers === "") {
-  alert("Campo vazio! Tente novamente.");
-} else {
-  isValidCard(insertCardNumbers);
-}
+do {
+  insertNumber = prompt('Insert the credit card number');
+} while (!insertNumber || typeof parseInt(insertNumber) !== 'number');
+isValidCard(insertNumber);
 
-function isValidCard(insertCardNumbers){
-  var reverseCardNumbers = []; 
-  var finalSum = 0; 
-  
-  for (i = 0; i < insertCardNumbers.length; i++) {
-    reverseCardNumbers.unshift(parseInt(insertCardNumbers[i]));
-  }
-
-  for (j = 0; j < reverseCardNumbers.length; j = j + 2) {
-    if (j % 2 === 0) {
-    reverseCardNumbers[j] = reverseCardNumbers[j] * 2;
-      if (reverseCardNumbers[j] >= 10) {
-      reverseCardNumbers[j] = reverseCardNumbers[j] - 9;
-      }
-    }
-
-    finalSum += reverseCardNumbers[j];
-  }
-
-  if (finalSum % 10 === 0) {
-    return alert("O seu cartão de crédito é válido");
-  } else {
-    return alert("O seu cartão de crédito é inválido");
-  }
- 
-
+function isValidCard(cardnumber) {
+  arr = cardnumber.split("").map( a => parseInt(a)).reverse();
+  const result = arr.map( (value, index) => (index % 2 == 1) ? ((value * 2 > 9) ? value * 2 - 9 : value * 2) : value).reduce ( (value, acum) => value + acum) % 10 == 0
+  result ? alert('Valid credit card number.') : alert('Invalid credit card number.')
 }
